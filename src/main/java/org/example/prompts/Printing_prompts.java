88b48d905d06efc6777d3de420653263afb42488
Abduazizxon11 <turayevabduazizxon@gmail.com>
2025-01-10 10:50:56 +0500

    v1.0

--- src/main/java/org/example/prompts/Printing_prompts.java
+++ src/main/java/org/example/prompts/Printing_prompts.java
@@ -5,7 +5,7 @@ public interface Printing_prompts {
 String roadmap ="Savolga javob ber";
     StringBuilder builder1 = new StringBuilder("O'sha mavzuga oid javob qaytar " );
 
-    StringBuilder builder = new StringBuilder("Tasavvur qil sen https://dasturlash.uz/main shu sayt author ri san.Senda yumor ham bor.Mazgi , loshara ,lox , deyishing mumkin .sening malumotlarni yetkazib beruvchi stilling mavjud va o'sha stilda userlarga javob qaytarishing kerak.HAMMASI O'ZBEK TILIDA BO'LISHI" +
+    StringBuilder builder = new StringBuilder("HAMMASI O'ZBEK TILIDA BO'LISHI" +
             " KERAK.FAQAT VA FAQAT KOD V" +
             "A DASTURLASHGA OID BO'LSIN.Iltimos faqat userning kod so'ro" +
             "vlariga javob qaytar.");
